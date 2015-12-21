0699fcba230f5bc90f15580448846cee91385390
Igor Afanasyev <afan@evernote.com>
2015-12-21 10:37:22 -0800

    Improve `test_language` plugin: - Better handling of URLs - Allow string expansion (disabled by default) - Allow to disable transliteration (enabled by default)

--- lib/Serge/Engine/Plugin/test_language.pm
+++ lib/Serge/Engine/Plugin/test_language.pm
@@ -6,7 +6,7 @@ use parent Serge::Plugin::Base::Callback;
 use strict;
 use warnings;
 
-use Serge::Util qw(set_flag);
+use Serge::Util qw(set_flags);
 
 our $LANG = 'test';
 our $LANGID = 0xffff;
@@ -23,6 +23,8 @@ sub init {
     $self->merge_schema({
         save_translations => 'BOOLEAN',
         language          => 'STRING',
+        transliterate     => 'BOOLEAN',
+        expand_length     => 'BOOLEAN',
         translations => {
             '*'           => 'STRING'
         }
@@ -31,6 +33,7 @@ sub init {
     $self->add({
         get_translation_pre => \&get_translation,
         get_translation => \&get_translation,
+        can_generate_ts_file => \&can_process_ts_file,
         can_process_ts_file => \&can_process_ts_file,
     });
 }
@@ -39,6 +42,8 @@ sub validate_data {
     my $self = shift;
 
     $self->SUPER::validate_data;
+
+    $self->{data}->{transliterate} = 1 unless exists $self->{data}->{transliterate};
 
     $self->{language} = exists $self->{data}->{language} ? $self->{data}->{language} : $LANG;
 }
@@ -49,10 +54,10 @@ sub adjust_phases {
     $self->SUPER::adjust_phases($phases);
 
     # always tie to 'can_process_ts_file' phase
-    set_flag($phases, 'can_process_ts_file');
+    set_flags($phases, 'can_generate_ts_file', 'can_process_ts_file');
 
-    # this plugin makes sense only when applied to a single phase (in addition to 'can_process_ts_file')
-    die "This plugin needs to be attached to only one phase at a time" unless @$phases == 2;
+    # this plugin makes sense only when applied to a single phase (in addition to 'can_generate_ts_file' and 'can_process_ts_file')
+    die "This plugin needs to be attached to only one phase at a time" unless @$phases == 3;
 }
 
 # public static method
@@ -67,57 +72,76 @@ sub _fake_translate_string {
 
     return $self->{data}->{translations}->{$s} if exists $self->{data}->{translations};
 
-    # Map English alphabet to similarly looking Unicode symbols with accents, umlauts, etc.
+    my $out = $s;
+    my $out_plaintext = $s;
 
-    my $map = {
-        'A' => "\x{100}",   'B' => "\x{1e02}",  'C' => "\x{106}",   'D' => "\x{110}",
-        'E' => "\x{112}",   'F' => "\x{1e1e}",  'G' => "\x{11c}",   'H' => "\x{124}",
-        'I' => "\x{128}",   'J' => "\x{134}",   'K' => "\x{136}",   'L' => "\x{141}",
-        'M' => "\x{1e3e}",  'N' => "\x{145}",   'O' => "\x{150}",   'P' => "\x{1e54}",
-        'Q' => "\x{3a9}",   'R' => "\x{156}",   'S' => "\x{160}",   'T' => "\x{166}",
-        'U' => "\x{16e}",   'V' => "\x{1e7c}",  'W' => "\x{174}",   'X' => "\x{4b2}",
-        'Y' => "\x{176}",   'Z' => "\x{17d}",   'a' => "\x{e1}",    'b' => "\x{1e03}",
-        'c' => "\x{e7}",    'd' => "\x{111}",   'e' => "\x{113}",   'f' => "\x{1e1f}",
-        'g' => "\x{11f}",   'h' => "\x{125}",   'i' => "\x{129}",   'j' => "\x{135}",
-        'k' => "\x{137}",   'l' => "\x{13c}",   'm' => "\x{1e3f}",  'n' => "\x{14b}",
-        'o' => "\x{151}",   'p' => "\x{1e55}",  'q' => "\x{1eb}",   'r' => "\x{155}",
-        's' => "\x{161}",   't' => "\x{163}",   'u' => "\x{169}",   'v' => "\x{1e7d}",
-        'w' => "\x{175}",   'x' => "\x{4b3}",   'y' => "\x{177}",   'z' => "\x{17e}"
-    };
+    my $has_plurr_like_formatting = $s =~ m/\{[^\}]*\{/;
 
-    # As PHP blocks can be inside tags, substitute them to ease parsing
-    $s =~ s/<\?(.*?)\?>/'__PHP__BLOCK__BEGIN__'._fake_translate_string_escape($1).'__PHP__BLOCK__END__'/ge;
+    if ($self->{data}->{transliterate} && !$has_plurr_like_formatting) {
+        # Map English alphabet to similarly looking Unicode symbols with accents, umlauts, etc.
 
-    my $sprintf = '%[-+]*[0 #]*[\d]*(?:.\d+)*(?:h|l|I|I32|I64)*[cCdiouxXeEfgGnsS]';
-    my $escape = '\\\\[ntvbrfax]'; # '\' is double-escaped
-    my $var = '%(?:\w+:)*\w+(?:&\w)*%';
-    my $tag = '<[^\?]>|<[^\?].*?[^\?]>'; # do not count <? and ?> inside tags
-    my $php = '__PHP__BLOCK__BEGIN__.*?__PHP__BLOCK__END__';
-    my $entity = '\&(?:\w+);';
-    my $javafmt = '\{\d+\}';
-    my $tplfmt = '\$\{.+?\}';
-    my $unixpath = '(?:^|[\/\r\n\t ]+)(?:\/[\w\d\.\%\?\-\=\,\&]+)+';
-    my @chunks = split(/($var|$sprintf|$escape|$php|$tag|$entity|$javafmt|$tplfmt|$unixpath)/, $s);
+        my $map = {
+            'A' => "\x{100}",   'B' => "\x{1e02}",  'C' => "\x{106}",   'D' => "\x{110}",
+            'E' => "\x{112}",   'F' => "\x{1e1e}",  'G' => "\x{11c}",   'H' => "\x{124}",
+            'I' => "\x{128}",   'J' => "\x{134}",   'K' => "\x{136}",   'L' => "\x{141}",
+            'M' => "\x{1e3e}",  'N' => "\x{145}",   'O' => "\x{150}",   'P' => "\x{1e54}",
+            'Q' => "\x{3a9}",   'R' => "\x{156}",   'S' => "\x{160}",   'T' => "\x{166}",
+            'U' => "\x{16e}",   'V' => "\x{1e7c}",  'W' => "\x{174}",   'X' => "\x{4b2}",
+            'Y' => "\x{176}",   'Z' => "\x{17d}",   'a' => "\x{e1}",    'b' => "\x{1e03}",
+            'c' => "\x{e7}",    'd' => "\x{111}",   'e' => "\x{113}",   'f' => "\x{1e1f}",
+            'g' => "\x{11f}",   'h' => "\x{125}",   'i' => "\x{129}",   'j' => "\x{135}",
+            'k' => "\x{137}",   'l' => "\x{13c}",   'm' => "\x{1e3f}",  'n' => "\x{14b}",
+            'o' => "\x{151}",   'p' => "\x{1e55}",  'q' => "\x{1eb}",   'r' => "\x{155}",
+            's' => "\x{161}",   't' => "\x{163}",   'u' => "\x{169}",   'v' => "\x{1e7d}",
+            'w' => "\x{175}",   'x' => "\x{4b3}",   'y' => "\x{177}",   'z' => "\x{17e}"
+        };
 
-    my $out;
-    my $translate;
-    foreach my $chunk (@chunks) {
-        $translate = !$translate;
+        # As PHP blocks can be inside tags, substitute them to ease parsing
+        $s =~ s/<\?(.*?)\?>/'__PHP__BLOCK__BEGIN__'._fake_translate_string_escape($1).'__PHP__BLOCK__END__'/ge;
 
-        if ($translate) {
-            my @chars = split(//, $chunk);
-            foreach my $char (@chars) {
-                my $c = $map->{$char};
-                $out .= $c ? $c : $char;
+        my $sprintf = '%[-+]*[0 #]*[\d]*(?:.\d+)*(?:h|l|I|I32|I64)*[cCdiouxXeEfgGnsS]';
+        my $escape = '\\\\[ntvbrfax]'; # '\' is double-escaped
+        my $var = '%(?:\w+:)*\w+(?:&\w)*%';
+        my $tag = '<[^\?]>|<[^\?].*?[^\?]>'; # do not count <? and ?> inside tags
+        my $php = '__PHP__BLOCK__BEGIN__.*?__PHP__BLOCK__END__';
+        my $entity = '\&(?:\w+);';
+        my $javafmt = '\{\d+\}';
+        my $tplfmt = '\$\{.+?\}';
+        my $unixpath = '(?:^|[\/\r\n\t ]+)(?:\/[\w\d\.\%\?\-\=\,\&]+)+';
+        my $url = '(?:http|https|ftp|ssh):\/\/[\/\w\d\.\%\?\-\=\,\&\+\#\@]+';
+        my $mailto = 'mailto:[\/\w\d\.\+\@]+';
+        my @chunks = split(/($var|$sprintf|$escape|$php|$tag|$entity|$javafmt|$tplfmt|$unixpath|$url|$mailto)/, $s);
+
+        $out = '';
+        $out_plaintext = '';
+        my $translate;
+        foreach my $chunk (@chunks) {
+            $translate = !$translate;
+
+            if ($translate) {
+                my @chars = split(//, $chunk);
+                foreach my $char (@chars) {
+                    $char = $map->{$char} || $char;
+                    $out .= $char;
+                    $out_plaintext .= $char;
+                }
+            } else {
+                $out .= $chunk;
             }
-        } else {
-            $out .= $chunk;
         }
+
+        # Substitute temporary markers back
+
+        $out =~ s/__PHP__BLOCK__BEGIN__(.*?)__PHP__BLOCK__END__/'<?'._fake_translate_string_unescape($1).'?>'/ge;
     }
 
-    # Substitute temporary markers back
+    # normalize plaintext string to see if it has
 
-    $out =~ s/__PHP__BLOCK__BEGIN__(.*?)__PHP__BLOCK__END__/'<?'._fake_translate_string_unescape($1).'?>'/ge;
+    $out_plaintext =~ s/\s{2,}/ /sg; # normalize whitespace
+    $out_plaintext =~ s/^\s+//sg; # trim left
+    $out_plaintext =~ s/\s+$//sg; # trim right
+
+    $out = _expand_string($out) if $self->{data}->{expand_length} && $out_plaintext ne '';
 
     return $out;
 }
@@ -140,6 +164,31 @@ sub _fake_translate_string_unescape {
     return $s;
 }
 
+sub _expand_string {
+    my $s = shift;
+
+    return $s if $s eq '';
+
+    my $count = length($s);
+
+    my $coeff = 1.4;
+    $coeff = 1.5 if $count < 200;
+    $coeff = 1.7 if $count < 20;
+    $coeff = 2 if $count < 10;
+
+    $count = int($count * ($coeff - 1));
+
+    my $dummy_text = ' XXXXXXXXXX';
+    # make sure dummy text length exceeds the needed number of chars to copy
+    while (length($dummy_text) < $count) {
+        $dummy_text .= $dummy_text;
+    }
+
+    my $start = my $stop = '!';
+
+    return $start.$s.substr($dummy_text, 0, $count).$stop;
+}
+
 sub get_translation {
     my ($self, $phase, $string, $context, $namespace, $filepath, $lang) = @_;
 
@@ -153,7 +202,7 @@ sub can_process_ts_file {
     # if this is a test language, do not import anything from translation file unless `save_translations' flag is on
     return 0 if $self->is_test_language($lang) && !$self->{data}->{save_translations};
 
-    # by default, allow to process any .po files for any given target language
+    # by default, allow to process any translation files for any given target language
     return 1;
 }
 
---
+++ t/data/engine/test_language/00/README.md
@@ -0,0 +1,1 @@
+Test for `test_language` plugin with `expand_length` option turned on
