5c3ad455bf6fe535bd437012082bc7dfa99e6b5a
Igor Afanasyev <afan@evernote.com>
2016-08-01 13:43:03 -0700

    Add new test for metaparser to parse PHP `define()`s

---
+++ t/data/engine/metaparser/02-php-defines/reference-output/localized-resources/test/sample.php
@@ -0,0 +1,10 @@
+<?php
+// file comment
+
+// comment for FOO message
+define("FOO", "Ḟőő");
+// comment for BAR message
+define("BAR", "Ḃáŕ");
+//define("BAZ", "old_baz");
+define("BAZ", "ŋēŵ_ḃáž");
+?>
---
+++ t/data/engine/metaparser/02-php-defines/resources/sample.php
@@ -0,0 +1,10 @@
+<?php
+// file comment
+
+// comment for FOO message
+define("FOO", "Foo");
+// comment for BAR message
+define("BAR", "Bar");
+//define("BAZ", "old_baz");
+define("BAZ", "new_baz");
+?>
