26dfbecef01455015c575d83b30de19da1984697
Igor Afanasyev <afan@evernote.com>
2017-11-29 10:01:58 -0800

    Fix rendering of notices/warnings/errors in `serge import` HTML reports

--- lib/Serge/Command/import.pm
+++ lib/Serge/Command/import.pm
@@ -69,24 +69,16 @@ sub run {
                 foreach my $row_data (@$file_data) {
                     $error_totals->{$row_data->{error_status}}++ if $row_data->{error_status};
                     my $severity = $row_data->{severity};
+                    my $key = $row_data->{key} ne '' ? $row_data->{key} : qq|<em>Empty key</em>|;
+                    my $status = $row_data->{error_status} ne '' ? qq|<span class="status">$row_data->{error_status}</span| : '';
 
-                    if ($row_data->{error_status} ne '') {
-                        $html .= qq|
-<tr class="key status $severity">
-    <td colspan="2">$row_data->{error_status}</td>
-</tr>
-|;
-                    } else {
-                        my $key = $row_data->{key} ne '' ? $row_data->{key} : qq|<em>Empty key</em>|;
-                        my $status = $row_data->{error_status} ne '' ? qq|<span class="status">$row_data->{error_status}</span| : '';
+                    map {
+                        $row_data->{$_} = xml_escape($row_data->{$_});
+                        $row_data->{$_} = '&nbsp;' if $row_data->{$_} eq '';
+                    } qw(key source translation);
 
-                        map {
-                            $row_data->{$_} = xml_escape($row_data->{$_});
-                            $row_data->{$_} = '&nbsp;' if $row_data->{$_} eq '';
-                        } qw(key source translation);
-
-                        my $class = qq| class="$severity"| if $severity ne '';
-                        $html .= qq|
+                    my $class = qq| class="$severity"| if $severity ne '';
+                    $html .= qq|
 <tr class="key $severity">
     <td colspan="2">$key$status</td>
 </tr>
@@ -96,7 +88,6 @@ sub run {
     <td>$row_data->{translation}</td>
 </tr>
 |;
-                    } # else
                 } # end foreach row
 
             } # end foreach key
