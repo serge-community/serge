b23b09916929bc3a8422614f286aef621ba3e2c7
Dragos Varovici <dvarovici.work@gmail.com>
2018-02-19 19:48:59 -0700

    Xliff settings and ability to store context as part of id

--- lib/Serge/Engine/Plugin/serialize_xliff.pm
+++ lib/Serge/Engine/Plugin/serialize_xliff.pm
@@ -15,8 +15,48 @@ sub name {
     return '.XLIFF 1.2 Serializer';
 }
 
+sub init {
+    my $self = shift;
+
+    $self->SUPER::init(@_);
+
+    $self->merge_schema({
+        use_hint_for_resname => 'BOOLEAN',
+        context_strategy => 'STRING',
+        valid_states => 'STRING',
+        file_datatype => 'STRING',
+        source_language => 'STRING',
+        state_translated => 'STRING',
+        state_new => 'STRING'
+    });
+}
+
+sub validate_data {
+    my $self = shift;
+
+    $self->SUPER::validate_data;
+
+    $self->{data}->{use_hint_for_resname} = 1 unless defined $self->{data}->{use_hint_for_resname};
+
+    $self->{data}->{context_strategy} = 'extradata' unless defined $self->{data}->{context_strategy};
+
+    $self->{data}->{file_datatype} = 'x-unknown' unless defined $self->{data}->{file_datatype};
+
+    if (($self->{data}->{context_strategy} ne 'id') and ($self->{data}->{context_strategy} ne 'extradata')) {
+        die "'context_strategy', which is set to $self->{data}->{context_strategy}, is not one of the valid options: 'id' or 'extradata'";
+    }
+
+    $self->{data}->{source_language} = 'en' unless defined $self->{data}->{source_language};
+
+    $self->{data}->{state_translated} = 'translated' unless defined $self->{data}->{state_translated};
+
+    $self->{data}->{state_new} = 'new' unless defined $self->{data}->{state_new};
+}
+
 sub serialize {
     my ($self, $units, $file, $lang) = @_;
+
+    my $use_hint_for_resname = $self->{data}->{use_hint_for_resname};
 
     my $locale = locale_from_lang($lang);
 
@@ -25,7 +65,7 @@ sub serialize {
             version => "1.2",
         });
 
-    my $file_element = $root_element->insert_new_elt('file' => {original => $file, 'source-language' => 'en', 'target-language' => $locale, datatype => 'x-unknown'}, '');
+    my $file_element = $root_element->insert_new_elt('file' => {original => $file, 'source-language' => $self->{data}->{source_language}, 'target-language' => $locale, datatype => $self->{data}->{file_datatype}}, '');
 
     my $body_element = $file_element->insert_new_elt('body');
 
@@ -34,23 +74,38 @@ sub serialize {
     foreach my $unit (@reversed_units) {
         my $approved = $unit->{fuzzy} ? "no" : "yes";
 
-        my $unit_element = $body_element->insert_new_elt('trans-unit' => {approved => $approved, id => $unit->{key}}, '');
+        my $unit_element = $body_element->insert_new_elt('trans-unit' => {approved => $approved}, '');
+
+        my $key = $unit->{key};
+
+        if ($unit->{context} ne '') {
+            if ($self->{data}->{context_strategy} eq 'extradata') {
+                $unit_element->set_att('extradata' => $unit->{context});
+            } elsif ($self->{data}->{context_strategy} eq 'id') {
+                $key .= ':'.$unit->{context};
+            }
+        }
+
+        $unit_element->set_att('id' => $key);
 
         my $dev_comment = $unit->{hint};
 
         if ($dev_comment ne '') {
             my @dev_comment_lines = split('\n', $dev_comment);
 
-            my $resname = $dev_comment_lines[0];
+            if ($use_hint_for_resname) {
+                my $resname = $dev_comment_lines[0];
 
-            $unit_element->set_att('resname' => $resname);
+                $unit_element->set_att('resname' => $resname);
 
-            my $dev_comment_lines_size = scalar @dev_comment_lines;
+                my $dev_comment_lines_size = scalar @dev_comment_lines;
 
-            if ($dev_comment_lines_size > 1) {
-                shift(@dev_comment_lines);
-            } else {
-                @dev_comment_lines = \();
+                if ($dev_comment_lines_size > 1) {
+                    shift(@dev_comment_lines);
+                }
+                else {
+                    @dev_comment_lines = \();
+                }
             }
 
             foreach my $dev_comment_line (reverse(@dev_comment_lines)) {
@@ -58,18 +113,16 @@ sub serialize {
             }
         }
 
-        if ($unit->{context} ne '') {
-            $unit_element->set_att('extradata' => $unit->{context});
-        }
-
         my $target_element = $unit_element->insert_new_elt('target' => {'xml:lang' => $locale}, $unit->{target});
 
         my $state = '';
 
-        if ($unit->{target} ne '') {
-            $state = $self->get_state($unit->{flags});
-        } else {
-            $state = 'new';
+        if ($self->{data}->{source_language} ne $lang) {
+            if ($unit->{target} ne '') {
+                $state = $self->get_state($unit->{flags});
+            } else {
+                $state = $self->{data}->{state_new};
+            }
         }
 
         if ($state ne '') {
@@ -89,11 +142,11 @@ sub serialize {
 sub get_state {
     my ($self, $unitflags) = @_;
 
-    return 'translated' unless defined $unitflags;
+    return $self->{data}->{state_translated} unless defined $unitflags;
 
     my @flags = @$unitflags;
 
-    my $state = 'translated';
+    my $state = $self->{data}->{state_translated};
 
     if (is_flag_set(\@flags, 'state-final')) {
         $state = 'final';
@@ -114,7 +167,11 @@ sub get_state {
 sub deserialize {
     my ($self, $textref) = @_;
 
-    my @valid_states = split(' ', 'translated final signed-off');
+    my @valid_states = \();
+
+    if ($self->{data}->{valid_states} ne '') {
+        @valid_states = split(' ', $self->{data}->{valid_states}); # translated final signed-off
+    }
 
     my @units;
 
@@ -138,7 +195,24 @@ sub deserialize {
 
     my @tran_units = $tree->findnodes('//trans-unit');
     foreach my $tran_unit (@tran_units) {
-        my $key = $tran_unit->att('id');
+        my $key = '';
+        my $context = '';
+
+        if ($self->{data}->{context_strategy} eq 'extradata') {
+            $key = $tran_unit->att('id');
+
+            $context = $tran_unit->att('extradata');
+        } elsif ($self->{data}->{context_strategy} eq 'id') {
+            my @id_parts = split(/:/, $tran_unit->att('id'));
+
+            $key = shift @id_parts;
+
+            my $id_parts_size = scalar @id_parts;
+
+            if ($id_parts_size > 0) {
+                $context = join(':', @id_parts);
+            }
+        }
 
         my $comment = '';
 
@@ -173,10 +247,19 @@ sub deserialize {
             $source = $source_element->text;
         }
 
-        my $context = $tran_unit->att('extradata');
         my $fuzzy = $tran_unit->att('approved') eq "no";
 
         next unless ($target or $comment);
+
+        if ($key eq '') {
+            print "\t\t? [empty key]\n";
+            next;
+        }
+
+        if ($key ne generate_key($source, $context)) {
+            print "\t\t? [bad key] $key for context $context\n";
+            next;
+        }
 
         if ($state ne '' and @valid_states) {
             my $is_valid_state = $state ~~ @valid_states;
@@ -185,11 +268,6 @@ sub deserialize {
                 print "\t\t? [invalid state] for $key for with state $state\n";
                 next;
             }
-        }
-
-        if ($key ne generate_key($source, $context)) {
-            print "\t\t? [bad key] $key for context $context\n";
-            next;
         }
 
         push @units, {
