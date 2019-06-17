a2db51d02be99a257005e4ef9ddcb2691dafe5b9
Igor Afanasyev <afan@evernote.com>
2019-06-17 15:27:30 -0700

    endpunc: make trailing punctuation more smart
    This change improves the guessing for strings that need to have
    trailing punctuation applied and wrapper tags added.
    
    Example: for strings like `<a>foo!</a>` the trailing punctuation
    is now properly detected as `!`.

--- lib/Serge/Engine/Plugin/Transform/endpunc.pm
+++ lib/Serge/Engine/Plugin/Transform/endpunc.pm
@@ -27,6 +27,15 @@ sub transform {
         $targetpunc = $1;
     }
 
+    if ($sourcepunc eq $targetpunc && $targetpunc eq '') {
+        # try to get rid of the trailing tags and whitespace to get the actual punctuation
+        my $filtered_target = $target;
+        $filtered_target =~ s/\s*<[^<>]+>\s*$//s;
+        if ($filtered_target ne $target) {
+            return $self->transform($s, $filtered_target, $lang);
+        }
+    }
+
     if ($sourcepunc ne $targetpunc) {
         # In Japanese, replace certain punctuation symbols
         if ($lang =~ m/^(ja|ja-.*)$/) {
