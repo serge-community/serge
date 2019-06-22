8ff6c2fc5c39d89d58788d95b4aed7bd5b016ca3
Igor Afanasyev <afan@evernote.com>
2019-06-21 21:03:19 -0700

    `feature_branch` plugin: disable optimizations for master files
    For the new approach with `log_translation` to work correctly,
    the files in the master job must always be parsed/generated
    (but not necessarily saved). This commit selectively disables
    optimizations to make sure the plugin works correctly in both
    forced and regular execution modes.

--- lib/Serge/Engine/Plugin/feature_branch.pm
+++ lib/Serge/Engine/Plugin/feature_branch.pm
@@ -23,6 +23,9 @@ sub init {
 
     $self->add({
         before_job => \&before_job,
+        before_generate_localized_files => \&before_generate_localized_files,
+        before_save_localized_file => \&before_save_localized_file,
+        after_save_localized_file => \&after_save_localized_file,
         can_extract => \&can_extract,
         get_translation_pre => \&get_translation_pre,
         log_translation => \&log_translation,
@@ -47,9 +50,13 @@ sub adjust_phases {
     $self->SUPER::adjust_phases($phases);
 
     # always tie to these phases
-    set_flags($phases, 'before_job', 'can_extract', 'get_translation_pre', 'log_translation');
+    set_flags($phases,
+        'before_job', 'before_generate_localized_files',
+        'before_save_localized_file', 'after_save_localized_file',
+        'can_extract', 'get_translation_pre', 'log_translation'
+    );
 
-    die "This plugin attaches itself to specific phases; please don't specify phases in the configuration file" unless @$phases == 4;
+    die "This plugin attaches itself to specific phases; please don't specify phases in the configuration file" unless @$phases == 7;
 }
 
 sub before_job {
@@ -81,6 +88,46 @@ sub before_job {
 
         $self->{master_translations} = $engine->{plugin_data}->{feature_branch}->{$master_job_id};
     }
+}
+
+sub before_generate_localized_files {
+    my ($self) = @_;
+
+    return () unless $self->{master_mode}; # do nothing in slave mode
+
+    # disable optimizations for localized file generation
+    # for a master job,  so that master files are always parsed,
+    # and their translations are always resolved and stored in
+    # `log_translation` phase. Since this is the last step
+    # before the job finishes execution, there's no need to restore
+    # optimizations mode after, but we still want to preserve
+    # optimizations at file saving time, so the original value
+    # of the job `optimizations` param is preserved here
+    # to be temporarily restored between `before_save_localized_file`
+    # and `after_save_localized_file` phases
+    $self->{optimizations} = $self->{parent}->{optimizations};
+    $self->{parent}->{optimizations} = undef;
+}
+
+sub before_save_localized_file {
+    my ($self) = @_;
+
+    return () unless $self->{master_mode}; # do nothing in slave mode
+
+    # restore the original value of `optimizations` job config value
+    # to avoid saving the localized file if it didin't change
+    $self->{parent}->{optimizations} = $self->{optimizations};
+}
+
+sub after_save_localized_file {
+    my ($self) = @_;
+
+    return () unless $self->{master_mode}; # do nothing in slave mode
+
+    print "::after_save_localized_file()\n";
+
+    # disable optimizations again
+    $self->{parent}->{optimizations} = undef;
 }
 
 # Make the key that best describes a given string;
